ff57a755f6b23895d4dfb935c80997e803120432
Matt Smith <msmith@apaxsoftware.com>
2016-09-02 00:53:37 -0400

    Define some basic actions to take.

--- lib/game_actions.py
+++ lib/game_actions.py
@@ -1,2 +1,6 @@
 def echo_action(request, response, game):
     response.addOutput(request.user_input)
+
+def exit_action(request, response, game):
+    response.addOutput("THANKS FOR PLAYING!!")
+    game.set_exit()
